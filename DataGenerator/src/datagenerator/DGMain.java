4e39d3f5692d34ae50587cb9845a457816b8b75b
Billy <bilorge@gmail.com>
2019-07-26 14:54:51 -0500

    Added a new project

---
+++ DataGenerator/src/datagenerator/DGMain.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datagenerator;
+
+/**
+ *
+ * @author bilorge
+ */
+public class DGMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
