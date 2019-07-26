8cca5faf399b678134295006ec7a4eb87d35a2ae
Billy <bilorge@gmail.com>
2019-07-26 17:36:37 -0500

    commit a for testing....

--- DataGenerator/src/datagenerator/DGMain.java
+++ DataGenerator/src/datagenerator/DGMain.java
@@ -1,5 +1,9 @@
 package datagenerator;
 
+import java.util.Date;
+
+
+
 /**
  *
  * @author bilorge
@@ -10,7 +14,13 @@ public class DGMain {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        DGMain me = new DGMain();
         System.out.println("Hi!!!!!");
+        me.SayDate();
+    }
+    
+    public void SayDate() {
+        System.out.println(new Date().toInstant().toString()); 
     }
     
 }
