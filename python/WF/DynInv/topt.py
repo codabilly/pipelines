4a82e63c68ba88a15703f73f5defb332209a7c2d
Billy <bilorge@gmail.com>
2019-08-05 17:21:21 -0500

    Finished the ansible custom inventory plugin.... man very disappointed with ansible for not having api reference docs... THEY HAVE TO WORK ON THAT.

--- python/WF/DynInv/topt.py
+++ python/WF/DynInv/topt.py
@@ -1,15 +1,36 @@
-import getopt
-import sys
+# import getopt
+# import sys
 
-argz = sys.argv[1:]
+# argz = sys.argv[1:]
+# # print(argz)
+
+# try:
+#       opts, args = getopt.getopt(argz,"h:d:")
+# except getopt.GetoptError:
+#     print 'usage: py.scrip -h <hostname> or -d <datacenter>'
+#     sys.exit(2)
+
 # print(argz)
+# print(args)
+# print(opts)
 
-try:
-      opts, args = getopt.getopt(argz,"h:d:")
-except getopt.GetoptError:
-    print 'usage: py.scrip -h <hostname> or -d <datacenter>'
-    sys.exit(2)
+# inventory = {'all': {}}
+# hosts = {'hosts': {}}
+# hosts.get('hosts').update({ 'paradox':{} })
+# print(hosts)
+# children = {'children': {}}
 
-print(argz)
-print(args)
-print(opts)
+# head = inventory.get('all')
+# head.update(hosts)
+# head.update(children)
+
+# print(inventory)
+# print(inventory.get('all').get('hosts'))
+
+groups = []
+
+groups.append('g1')
+groups.append('b72')
+groups.append('c3')
+
+print(groups)
